085c8918505ef2578ecab463151effe83182b3a0
Maurice Chevez <82402602+mauricechevez@users.noreply.github.com>
2021-05-14 15:58:40 -0400

    Initial commit

---
+++ controllers/auth.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('../config/ppConfig');
+const db = require('../models')
+
+router.get('/signup', (req, res) => {
+  res.render('auth/signup');
+});
+
+router.get('/login', (req, res) => {
+  res.render('auth/login');
+});
+
+router.post('/login', passport.authenticate('local',{
+  //handles the logic if pass or fail
+  successRedirect: '/',
+  failureRedirect:'/auth/login',
+  successFlash: 'Welcome Back!',
+  failureFlash: " Your email or password is incorrect. Please try again."
+}))
+
+router.post('/signup', async(req,res)=>{
+  const {name, email, password} = req.body;
+
+  try{
+    const [user,created] = await db.user.findOrCreate({
+      where: {email},
+      defaults: {name,password}
+    })
+    if (created){
+      console.log(`--------- ${user.name} was created`)
+      const successObject = {
+        successRedirect: '/',
+        successFlash: `Welcome ${user.name}. Account was created`
+      }
+
+      passport.authenticate('local',successObject)(req,res)
+    } else{
+      req.flash('error', 'Email already exists')
+      req.redirect('/auth/signup')
+    }
+  } catch (error){
+    console.log(`------------ ERROR ------------`)
+    console.log(error)
+    //Handle the user experience if something goes wrong
+    req.flash('error','Either email or password is incorrect. Please try again')
+    res.redirect('/auth/signup')
+  }
+})
+
+router.get('/logout', (req, res) => {
+  req.logOut(); // logs the user out of the session
+  req.flash('success', 'Logging out... See you next time!');
+  res.redirect('/');
+});
+
+module.exports = router;
---
+++ models/user.js
@@ -0,0 +1,67 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+const {
+  Model, STRING
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class user extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  user.init({
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+       len: {
+        args: [1,99],
+        msg: 'Name must be between 1 and 99 characters'
+       }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate:{
+        isEmail:{
+          msg: 'Invalid Email'
+        }
+      }
+    },
+    password: {
+      type:DataTypes.STRING,
+      validate:{
+        len:{
+          args:[8,99],
+          msg: 'Password must be between 8 and 99 characters'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'user',
+  });
+// Hash the password using the bsync library
+user.addHook('beforeCreate', (pendingUser) =>{
+  let hash = bcrypt.hashSync(pendingUser.password, 12)
+  pendingUser.password = hash // this hashed pw goes to the DB
+})
+user.prototype.validPassword = function(typedPassword){
+  let isCorrectPassword = bcrypt.compareSync(typedPassword, this.password)
+  return isCorrectPassword;
+}
+// return an object from the DB of the user model, without the hashed pw.
+
+user.prototype.toJSON = function(){
+    let userData = this.get()
+    delete userData.password;
+  return userData
+}
+
+  return user;
+};

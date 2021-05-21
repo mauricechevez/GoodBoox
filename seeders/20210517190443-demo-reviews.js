36060046a8bd2e47a4d94c87382e8d0bb777c001
mchevez <mauricechevez@gmail.com>
2021-05-21 12:31:56 -0400

    Heroku: Update Seed file for Reviews

--- seeders/20210517190443-demo-reviews.js
+++ seeders/20210517190443-demo-reviews.js
@@ -6,6 +6,7 @@ const reviewData = [
     title: "Romeo and Juliet",
     content:`"This great book (drama of course) I read in a single night. Naturally, an English graduate seldom can remain away from Shakespeare and his realm. However, even as an individual, before I began my studies seriously, Shakespeare and some of his creations were on the list 'to be read'. Romeo and Juliet is a play, to be clear at the beginning. Yes, as critics (modern ones) claim, this is perhaps the most 'unlikely' play which does not synchronise with the reality as others by the same dramatist. Nevertheless, let's give the 'play' its due - it surely does create that sensation which Shakespeare wanted to. The ephemeral romance between the 'first sight lovers' and the enemies sworn to suck the blood out of their lives... everything went on perfectly to (at least) create the star of today's Hollywood - Leo!
     The book has its merits as well as the demerits. Shakespeare is the vacuum. You can keep your experiments going on... I would like to rather appreciate him for his creation this time. I enjoyed reading the play and truly did!"`,
+    userId: 1,
     book_rating:5,
     book_price:4,
     img_url:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1572098085l/18135._SY475_.jpg",
@@ -18,6 +19,7 @@ const reviewData = [
     //2nd Book Review
     title: "Dracula",
     content:"I was rather disappointed by this classic. It started out with promise, especially the Jonathan Harker bits. Then all the male characters descended into blubbering worshippers of the two female characters, and by the end of the novel, I was wishing Dracula could snack on all of them and be done with it. I kept having to put it aside and read chapters in between other books, but I managed to finish it at last",
+    userId: 1,
     book_rating:2,
     book_price:4,
     img_url:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1572098085l/18135._SY475_.jpg",
@@ -31,6 +33,7 @@ const reviewData = [
     //3rd Book Review
     title:"Without Remorse",
     content:`"This book is good for three reasons. One, it's not a Jack Ryan story. It tells us how John Clark came into being, who he is and what he is, and the plot actually happens roughly 15-20 years before the main series. Two, Vietnam, so it's low tech and gritty Ramboism. Three, Tom Clancy normally wrote Puritan Republican-voting cardboard emotions for his characters with the full knowledge that they would be portrayed in the cinema, so it must look good. But here, he went the distance with John and actually gave him something of a semblance of a real personality, and it's one of his more engaging works. All combined, it's a very decent, classic thriller, and you will like for a bunch of reasons, especially since John is sort of an anti-hero, and there are several unique, old era elements in the book, which make for a very refreshing departure from Tom's highly dense and detailed military flavored writing."`,
+    userId: 1,
     book_rating:5,
     book_price:10,
     img_url:"https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1310082706l/19668.jpg",
@@ -42,6 +45,7 @@ const reviewData = [
   {
     title:"I Am Not Spock",
     content:`I am a fan of Star Trek, in particular the original series movies. I've also had occasion to listen to Mr. Nimoy speak twice, so I was looking forward to reading this infamously titled autobiography. I wasn't disappointed, and the 'voice' in the book matched up with my expectations for the most part. Not a heavy ready, it's a pretty slim book, but I enjoyed it quite a bit. I don't know that I'd recommend it to the rabid Trekkie, since it really isn't focused on the series. I'm about to start on 'I Am Spock' next, and I'm looking forward to seeing how Mr. Nimoy's writing developed from one book to the next.`,
+    userId: 1,
     book_rating:4,
     book_price:10,
     img_url:'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1331448368l/580882.jpg',
